2a13fdc73a1ab7f40a8a9cb6594379e84ef22c89
Abhisek Sahoo <abhiseksahoo33@gmail.com>
2024-03-20 20:12:13 +0530

    Day-3: Colored text in CLI o/p

--- GO-lang_Practice/Day3/3.Package/DemoPackage/main.go
+++ GO-lang_Practice/Day3/3.Package/DemoPackage/main.go
@@ -6,6 +6,9 @@ import (
 	adv "first/math/advanced" // adv is the package allias of advanced
 	"fmt"
 	"math"
+
+	"github.com/fatih/color"
+	. "github.com/fatih/color" // Bad Practice can use [Cyan("This is from external package")]
 	// _ "anypackage name"  if you dont want the package to remove while saving either give alias or place _ prefix
 )
 
@@ -15,7 +18,8 @@ func init() {
 
 func main() {
 	fmt.Println("This is my Package and module Demo")
-
+	color.Cyan("This is from external package") //will print the output in Cyan coloured text
+	Cyan("This is from external package")       // Bad Practice
 	fmt.Println("Result from math: ", math.Sin(0.123))
 
 	fmt.Println(mmath.Add(1, 2))
@@ -33,6 +37,8 @@ func main() {
 	//fmt.Println(mmath.pI) #ERROR
 
 	fmt.Println(mmath.CircleProperties(2.4))
+
+	color.Red("This from Exit package") //will print the output in Red coloured text
 }
 
 /*
